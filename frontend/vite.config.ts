471281064771822a2eed4f018d3203d7a40ea099
Tachiguishi <TachiguishiRor@gmail.com>
2026-01-13 09:12:32 +0800

    feat: add auto-import and component plugins for Vue and Naive UI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,28 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useMessage', 'useDialog', 'useNotification', 'useLoadingBar'],
+        },
+      ],
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
